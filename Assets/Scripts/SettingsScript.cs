a092f36a10b0709fd4f197f0c112201acb162502
LilloEJ <150400456+LilloEJ@users.noreply.github.com>
2024-12-04 19:05:34 +0800

    added script for Audio, Display, and Credits btns

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class SettingsScript : MonoBehaviour
 {
-    public Behaviour objBehavior;
-
-
-    public void Settings()
+    public GameObject AudioCanvas;
+    public GameObject DisplayCanvas;
+    public GameObject CreditsCanvas;
+    public void Click_Audio()
     {
-        objBehavior.enabled= !objBehavior.enabled;
+        AudioCanvas.SetActive(true);
+        DisplayCanvas.SetActive(false);
+        CreditsCanvas.SetActive(false);
+    }
+
+    public void Click_Display()
+    {
+        AudioCanvas.SetActive(false);
+        DisplayCanvas.SetActive(true);
+        CreditsCanvas.SetActive(false);
+    }
+
+    public void Click_Credits()
+    {
+        AudioCanvas.SetActive(false);
+        DisplayCanvas.SetActive(false);
+        CreditsCanvas.SetActive(true);
     }
 }
