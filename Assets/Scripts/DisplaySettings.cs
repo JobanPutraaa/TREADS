9279b64402485832d618d4aea01512fcf3b9f996
LilloEJ <150400456+LilloEJ@users.noreply.github.com>
2024-12-05 00:56:15 +0800

    updated DisplayScript

--- Assets/Scripts/DisplaySettings.cs
+++ Assets/Scripts/DisplaySettings.cs
@@ -12,6 +12,7 @@ public class DisplaySettings : MonoBehaviour
     Resolution[] Allresolutions;
     public bool isFullScreen;
     public int SelectedRes;
+    List<Resolution> SelectedResList = new List<Resolution>();
 
     public void Start()
     {
@@ -19,17 +20,33 @@ public class DisplaySettings : MonoBehaviour
         Allresolutions = Screen.resolutions;
 
         List<string> list = new List<string>();
-
-        foreach(Resolution res in Allresolutions)
+        string newRes;
+        foreach (Resolution res in Allresolutions)
         {
-            list.Add(res.ToString());
+            newRes = res.width.ToString() + " X " + res.height.ToString();
+            if(!list.Contains(newRes))
+            {
+                list.Add(newRes);
+                SelectedResList.Add(res);
+            }
         }
 
         resolutionDropdown.AddOptions(list);
     }
+
+    public void changeRes()
+    {
+        SelectedRes = resolutionDropdown.value;
+        Screen.SetResolution(SelectedResList[SelectedRes].width, SelectedResList[SelectedRes].height, isFullScreen);
+    }
+
+    public void ChangeFullScreen()
+    {
+        isFullScreen = fullToggle.isOn;
+        Screen.SetResolution(SelectedResList[SelectedRes].width, SelectedResList[SelectedRes].height, isFullScreen);
+    }
     void Update()
     {
-        
 
     }
 }
