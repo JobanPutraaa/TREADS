0e94ec42cfb249c62a7a6e2adf539ffd12036110
LilloEJ <150400456+LilloEJ@users.noreply.github.com>
2024-12-04 19:53:07 +0800

    Added Methods to control audio

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -11,5 +11,38 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider sfxSlider;
 
+    public void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVol();
+        }
+        else
+        {
+            setMusicVol();
+        }
+    }
+    public void setMusicVol()
+    {
+        float volume = musicSlider.value;
+        audioMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+    }
 
+    public void setMasterVolume()
+    {
+        float volume = masterSlider.value;
+        audioMixer.SetFloat("master", Mathf.Log10(volume) * 20);
+    }
+
+    public void setSFXVolume()
+    {
+        float volume = sfxSlider.value;
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    }
+
+    private void LoadVol()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        setMusicVol();
+    }
 }
